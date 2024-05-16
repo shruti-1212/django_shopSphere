5209a8859e0238b019be234c19a923bc044f35c1
shruti.hande <shrutihande0@gmail.com>
2024-05-16 19:07:49 -0400

    adding item details

--- shopSphere/shopSphere/urls.py
+++ shopSphere/shopSphere/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import index, contact
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name='index'),
+    path('items/',include('item.urls')),
     path('contact/', contact, name='contact'), 
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
